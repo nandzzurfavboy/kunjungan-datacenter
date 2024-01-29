9478a7ded28d7b7828087838506dc4550913bc7a
Ananda Aqilla Aqsa <132327112+nandzzurfavboy@users.noreply.github.com>
2024-01-29 12:10:01 +0700

    responsive blum fix

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,10 @@
-<a href="?reservasi" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<!-- <a href="?reservasi" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
 <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-</a>
+</a> -->
+
+<div class="container px-6 max-w-md">
+    <h1 class="text-2xl font-bold font-inter text-slate-800">Halo Selamat Datang,</h1>
+    <p class="mt-2">Anda berada di halaman Reservasi Kunjungan Data Center <span>DISKOMINFO PROVINSI SUMATERA UTARA</span>. Anda dapat mengajukan permohonan kunjungan dengan mengisi data diri pada <a href="?reservasi">Form Permohonan Kunjungan</a>, atau mengecek status permohonan anda pada halaman <a href="">Cek Status Permohonan</a>.</p>
+</div>
